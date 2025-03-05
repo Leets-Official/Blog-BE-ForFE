803566ddd307d6b97d0d065346eff196a49de8f2
rootTiket <l20078829@gmail.com>
2025-03-05 17:16:46 +0900

    fix: jpa 매핑 수정

--- src/main/java/com/blog/domain/board/domain/repository/PostRepository.java
+++ src/main/java/com/blog/domain/board/domain/repository/PostRepository.java
@@ -4,5 +4,5 @@ import com.blog.domain.board.domain.entity.Post;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<UUID, Post> {
+public interface PostRepository extends JpaRepository<Post, UUID> {
 }
--- src/main/java/com/blog/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/blog/domain/comment/domain/repository/CommentRepository.java
@@ -1,4 +1,7 @@
 package com.blog.domain.comment.domain.repository;
 
-public interface CommentRepository {
+import com.blog.domain.comment.domain.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
