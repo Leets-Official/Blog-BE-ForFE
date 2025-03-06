84c14090a1ed0fef502190d312aa3d2e1d58da90
rootTiket <l20078829@gmail.com>
2025-03-06 12:17:57 +0900

    feat: 게시물 조회 로직 구현

--- src/main/java/com/blog/domain/board/domain/repository/PostRepository.java
+++ src/main/java/com/blog/domain/board/domain/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package com.blog.domain.board.domain.repository;
 
 import com.blog.domain.board.domain.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
+    Page<Post> findAll(Pageable pageable);
 }
--- src/main/java/com/blog/domain/board/domain/service/PostGetService.java
+++ src/main/java/com/blog/domain/board/domain/service/PostGetService.java
@@ -3,8 +3,12 @@ package com.blog.domain.board.domain.service;
 import com.blog.domain.board.domain.entity.Post;
 import com.blog.domain.board.domain.repository.PostRepository;
 import com.blog.domain.board.exception.PostNotFoundException;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +18,10 @@ public class PostGetService {
 
     public Post find(UUID postId) {
         return postRepository.findById(postId).orElseThrow(PostNotFoundException::new);
+    }
+
+    public List<Post> findAll(int size, int page) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+        return postRepository.findAll(pageable).getContent();
     }
 }
