815345932ea734806b335e83cfe6afa3414aa96d
rootTiket <l20078829@gmail.com>
2025-03-06 11:19:29 +0900

    feat: 단일 게시물 조회 기능 추가

--- src/main/java/com/blog/domain/board/application/usecase/PostManageUsecase.java
+++ src/main/java/com/blog/domain/board/application/usecase/PostManageUsecase.java
@@ -1,10 +1,14 @@
 package com.blog.domain.board.application.usecase;
 
 import com.blog.domain.board.application.dto.PostCreateDto;
+import com.blog.domain.board.application.dto.PostReadResponse;
 import com.blog.domain.board.domain.entity.Post;
+import com.blog.domain.board.domain.service.PostGetService;
 import com.blog.domain.board.domain.service.PostSaveService;
+import com.blog.domain.board.domain.service.PostValidateService;
 import com.blog.domain.user.domain.entity.User;
 import com.blog.domain.user.domain.service.UserGetService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +18,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostManageUsecase {
     private final UserGetService userGetService;
     private final PostSaveService postSaveService;
+    private final PostGetService postGetService;
+    private final PostValidateService postValidateService;
 
     @Transactional
     public void createPost(Long userId, PostCreateDto dto) {
@@ -21,5 +27,15 @@ public class PostManageUsecase {
         Post post = Post.CreatePost(dto.title(), dto.content(), dto.image(), user);
 
         postSaveService.save(post);
+    }
+
+    @Transactional(readOnly = true)
+    public PostReadResponse readPost(Long userId , UUID postId) {
+        User user = userGetService.find(userId);
+        Post post = postGetService.find(postId);
+
+        boolean isOwner = postValidateService.certificate(post,user);
+
+        return PostReadResponse.toResponse(post, isOwner);
     }
 }
--- src/main/java/com/blog/domain/board/presentation/PostController.java
+++ src/main/java/com/blog/domain/board/presentation/PostController.java
@@ -1,15 +1,19 @@
 package com.blog.domain.board.presentation;
 
 import static com.blog.domain.board.presentation.constant.ResponseMessage.CREATE_SUCCESS;
+import static com.blog.domain.board.presentation.constant.ResponseMessage.READ_SUCCESS;
 
 import com.blog.domain.board.application.dto.PostCreateDto;
+import com.blog.domain.board.application.dto.PostReadResponse;
 import com.blog.domain.board.application.usecase.PostManageUsecase;
 import com.blog.global.common.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -28,5 +32,12 @@ public class PostController {
     public ResponseDto<Void> create(@RequestHeader Long userId, @RequestBody PostCreateDto dto) {
         postManageUsecase.createPost(userId, dto);
         return ResponseDto.of(HttpStatus.CREATED.value(), CREATE_SUCCESS.getMessage());
+    }
+
+    @GetMapping
+    @Operation(summary = "게시물 조회")
+    public ResponseDto<PostReadResponse> read(@RequestHeader Long userId, @RequestHeader UUID postId) {
+        PostReadResponse response = postManageUsecase.readPost(userId, postId);
+        return ResponseDto.of(HttpStatus.OK.value(), READ_SUCCESS.getMessage(), response);
     }
 }
