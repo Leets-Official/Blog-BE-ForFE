7bcfed5806d4b8a1cc405b58e08b51bcc2747e81
rootTiket <l20078829@gmail.com>
2025-03-06 15:22:41 +0900

    feat: 본인 소유 검사 메서드

--- src/main/java/com/blog/domain/board/domain/service/PostValidateService.java
+++ src/main/java/com/blog/domain/board/domain/service/PostValidateService.java
@@ -2,6 +2,7 @@ package com.blog.domain.board.domain.service;
 
 import com.blog.domain.board.domain.entity.Post;
 import com.blog.domain.board.domain.repository.PostRepository;
+import com.blog.domain.board.exception.PostAccessDeniedException;
 import com.blog.domain.user.domain.entity.User;
 import com.blog.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +11,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PostValidateService {
-    private final PostRepository postRepository;
-    private final UserRepository userRepository;
 
-    public boolean certificate(Post post, User user) {
-        return post.getUser().equals(user);
+    public void certificate(Post post, User user) {
+        if (!post.getUser().equals(user)) {
+            throw new PostAccessDeniedException();
+        }
     }
 }
