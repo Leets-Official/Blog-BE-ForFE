7824d15c5e69e8e7b4d2db2e438cedc6dc4eef41
rootTiket <l20078829@gmail.com>
2025-03-06 22:31:17 +0900

    feat: 댓글 수정 기능 추가

--- src/main/java/com/blog/domain/comment/application/usecase/CommentManageUsecase.java
+++ src/main/java/com/blog/domain/comment/application/usecase/CommentManageUsecase.java
@@ -3,8 +3,12 @@ package com.blog.domain.comment.application.usecase;
 import com.blog.domain.board.domain.entity.Post;
 import com.blog.domain.board.domain.service.PostGetService;
 import com.blog.domain.comment.application.dto.CommentCreateDto;
+import com.blog.domain.comment.application.dto.CommentUpdateDto;
 import com.blog.domain.comment.domain.entity.Comment;
+import com.blog.domain.comment.domain.service.CommentGetService;
 import com.blog.domain.comment.domain.service.CommentSaveService;
+import com.blog.domain.comment.domain.service.CommentUpdateService;
+import com.blog.domain.comment.domain.service.CommentValidateService;
 import com.blog.domain.user.domain.entity.User;
 import com.blog.domain.user.domain.service.UserGetService;
 import java.util.UUID;
@@ -18,6 +22,9 @@ public class CommentManageUsecase {
     private final CommentSaveService commentSaveService;
     private final UserGetService userGetService;
     private final PostGetService postGetService;
+    private final CommentGetService commentGetService;
+    private final CommentValidateService commentValidateService;
+    private final CommentUpdateService commentUpdateService;
 
     @Transactional
     public void createComment(Long userId,UUID postId, CommentCreateDto dto) {
@@ -26,5 +33,15 @@ public class CommentManageUsecase {
         Comment comment = Comment.of(dto.content(), post, user);
 
         commentSaveService.save(comment);
+    }
+
+    @Transactional
+    public void updateComment(Long userId, Long commentId, CommentUpdateDto dto) {
+        User user = userGetService.find(userId);
+        Comment comment = commentGetService.find(commentId);
+
+        commentValidateService.certificate(comment, user);
+
+        commentUpdateService.update(comment,dto);
     }
 }
--- src/main/java/com/blog/domain/comment/presentation/CommentController.java
+++ src/main/java/com/blog/domain/comment/presentation/CommentController.java
@@ -2,8 +2,10 @@ package com.blog.domain.comment.presentation;
 
 
 import static com.blog.domain.comment.presentation.constant.ResponseMessage.CREATE_SUCCESS;
+import static com.blog.domain.comment.presentation.constant.ResponseMessage.UPDATE_SUCCESS;
 
 import com.blog.domain.comment.application.dto.CommentCreateDto;
+import com.blog.domain.comment.application.dto.CommentUpdateDto;
 import com.blog.domain.comment.application.usecase.CommentManageUsecase;
 import com.blog.global.common.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +13,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,8 +30,17 @@ public class CommentController {
 
     @PostMapping("{postId}")
     @Operation(summary = "댓글 작성")
-    public ResponseDto<Void> create(@RequestHeader Long userId, @PathVariable UUID postId , @RequestBody CommentCreateDto dto) {
+    public ResponseDto<Void> create(@RequestHeader Long userId, @PathVariable UUID postId,
+                                    @RequestBody CommentCreateDto dto) {
         commentManageUsecase.createComment(userId, postId, dto);
         return ResponseDto.of(HttpStatus.CREATED.value(), CREATE_SUCCESS.getMessage());
+    }
+
+    @PatchMapping("{commentId}")
+    @Operation(summary = "댓글 수정")
+    public ResponseDto<Void> update(@RequestHeader Long userId, @PathVariable Long commentId,
+                                    @RequestBody CommentUpdateDto dto) {
+        commentManageUsecase.updateComment(userId, commentId, dto);
+        return ResponseDto.of(HttpStatus.CREATED.value(), UPDATE_SUCCESS.getMessage());
     }
 }
