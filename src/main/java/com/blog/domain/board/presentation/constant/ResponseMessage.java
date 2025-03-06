b2ed8d10245944909577d3e1583d1ee13f069bb4
rootTiket <l20078829@gmail.com>
2025-03-06 11:18:34 +0900

    feat: 조회 예외 추가

--- src/main/java/com/blog/domain/board/presentation/constant/ResponseMessage.java
+++ src/main/java/com/blog/domain/board/presentation/constant/ResponseMessage.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ResponseMessage {
-    CREATE_SUCCESS("게시물 생성에 성공했습니다");
+    CREATE_SUCCESS("게시물 생성에 성공했습니다"),
+    READ_FAILED("게시물 조회에 실패했습니다"),
+    READ_SUCCESS("게시물 조회에 성공했습니다");
 
     private final String message;
 
